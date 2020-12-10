287cf317608a70b593d7cead47bfc70a81fbf5ac
mc-cat-tty <>
2020-12-10 10:05:40 +0100

    added day1 code

---
+++ day1_1.py
@@ -0,0 +1,23 @@
+"""
+Day 1, part 1 - Report Repair
+
+https://adventofcode.com/2020/day/1
+
+Usage: pipe your input into the script
+"""
+
+__author__ = "Francesco Mecatti"
+
+import sys
+
+def main():
+    numList = list(sys.stdin)
+    for num1 in numList:
+        for num2 in numList:
+            if int(num1)+int(num2) == 2020:
+                print(f"{int(num1)}*{int(num2)} = {int(num1)*int(num2)}")
+                return
+
+
+if __name__ == "__main__":
+    main()

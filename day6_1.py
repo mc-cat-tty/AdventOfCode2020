5a0b8df49a7c2c853952e3eaeab4389f2030cfba
mc-cat-tty <>
2020-12-24 13:18:33 +0100

    solved day 6 part 1

--- day6_1.py
+++ day6_1.py
@@ -4,6 +4,7 @@ Day 6, part 1 - Custom Customs
 https://adventofcode.com/2020/day/6
 
 Usage: pipe your input into the script
+Keep in mind that you have to add a blank line at the end of the file, otherwise the last group will be ignored
 """
 
 __author__ = "Francesco Mecatti"
@@ -12,20 +13,17 @@ import sys
 import re
 
 def main():
+    s: Set[str] = set()  # Answers set
+    count: int = 0
     for line in sys.stdin:
         line = line.strip()
         if not line:  # Blank line
-            continue
-        r_str: str = line[:7]  # Row string identifier
-        c_str: str = line[7:]  # Col string identifier
-        r_str_bin:str = ''.join(list(map(lambda x: '0' if x == 'F' else '1', r_str)))
-        r_id: int = int(r_str_bin, 2)
-        c_str_bin:str = ''.join(list(map(lambda x: '0' if x == 'L' else '1', c_str)))
-        c_id: int = int(c_str_bin, 2)
-        current_id: int = r_id * 8 + c_id
-        max_id = max(max_id, current_id)
-
-    print(max_id)
+            count += len(s)
+            s = set()  # Reset the set :)
+        else:  # Line is an answer that belongs to the current group
+            l: List[str] = list(line)
+            s.update(*l)
+    print(count)
 
 
 if __name__ == "__main__":
